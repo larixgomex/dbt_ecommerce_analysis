1b307d6b6fa0421fb5e084a16574f87377026dfb
Lari Gomes <goodboyvincenzo@gmail.com>
2023-12-05 20:43:31 +0000

    created kpi and kpi top tables

---
+++ models/int_cc_stock_kpi_top.sql
@@ -0,0 +1,9 @@
+select
+    model_type,
+    count(in_stock) as nb_products,
+    sum(in_stock) as nb_products_in_stock,
+    round(avg(1 - in_stock), 3) as shortage_rate,
+    sum(stock_value) as stock_value,
+    sum(stock) as stock
+from {{ ref("cc_stock_kpi") }}
+group by model_type
