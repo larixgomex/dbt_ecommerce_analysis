d0bb5ad109ae1016e0c84955e6bf2fbeebc17d5f
Lari Gomes <goodboyvincenzo@gmail.com>
2023-12-12 12:43:44 +0000

    docs generated

--- models/marts/_sales_daily.sql
+++ models/marts/_sales_daily.sql
@@ -1,4 +1,4 @@
 select product_id, sum(qty) as qty_91, round(sum(qty) / 91, 2) as avg_daily_qty_91
-from {{ ref("cc_sales") }}
+from {{ ref('_cc_sales') }}
 where date_date >= date_sub('2021-10-01', interval 91 day)
 group by product_id
